60d3281008018705985c46eccf7ba1a40bbe662c
Wang Zixu <3043382926@qq.com>
2022-04-11 12:38:08 +0800

    initial mode ( already finish IB before coding )

---
+++ PicDemo/Controllers/SearchViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SearchViewController.swift
+//  PicDemo
+//
+//  Created by 王梓旭 on 2022/4/10.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
